8bc68ddb04f35aa574c262c3391c1f49a69a9a23
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-01-25 14:22:32 +0300

    Implement Temp01 and Temp02 classes for data types
    Added two classes Temp01 and Temp02 demonstrating various data types and their usage in Java.

---
+++ data types.java
@@ -0,0 +1,70 @@
+package data_types;
+
+public class Temp01
+{
+	public static void main(String[] asdfg) 
+	{
+		System.out.println();  // print(, end="\n")
+		//System.out.print();  // print(, end="")
+		
+		System.out.println("Hello \nWo\trld!"); System.out.println('J');
+		
+	}
+}
+
+/*
+
+*/
+
+package data_types;
+
+public class Temp02 
+{
+	public static void main(String[] args)
+	{
+		/*
+		a = 5	
+		a = 4.5
+		a = 'adjhkja'
+		a = "fghjkl"
+		a = True
+		*/
+		
+		int a = 5;
+		a = 6;
+		a = 65;
+		
+		//a = 4.5; not allowed
+		double b = 4.55;
+		float c = 4.55f; // float < double
+		
+		System.out.println(a * b);
+		// byte < short < int < long
+		
+		
+		char d = 'J';
+		System.out.println(d);
+		int e = d;
+		System.out.println(e);
+		char f = (char)a;
+		System.out.println(f);
+		
+		boolean g = true;
+		
+		String s = "java";
+		
+		System.out.println(s);
+	}	
+
+}
+
+
+
+
+
+
+
+
+
+
+
